27c5d1d4e4a2ca0da32f781c0f15ffcfdb23e1d4
Dani Checa <dani89.cm@gmail.com>
2017-07-09 13:14:00 +0200

    Fetch & render data

--- assets/main.js
+++ assets/main.js
@@ -4,7 +4,17 @@ $(function() {
     url: 'https://www.codeschool.com/users/danichk.json',
     dataType: 'jsonp',
     success: function(response) {
-      // handle response
+      var output = '';
+
+      $(response.courses.completed).each(function () {
+        var title = '<h3 class="title">' + this.title + '</h3>';
+        var badge = '<img src="' + this.badge + '" alt="' + this.title + '">';
+        var link = '<a href="' + this.url + '" target="_blank" class="btn btn-primary">See Course</a>';
+
+        output += '<div class="course">' + title + badge + link + '</div>';
+      });
+
+      $('#badges').append(output);
     }
   });
 
